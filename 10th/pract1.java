a530f0844f6b1497f342c190ef3f13a3a147f7fb
Snehal Adbol <smitaadbol01@gmail.com>
2023-07-27 13:14:44 +0530

    Update pract1.java

--- 10th/pract1.java
+++ 10th/pract1.java
@@ -1,1 +1,20 @@
+package tenth;
+/* 1
+ * 22
+ * 333
+ * 4444
+ * 55555
+ * 
+ * Question from rt */
 
+public class pract1 {
+	public static void main (String []args) {
+int range = 5; 
+for(int i = 1; i<=range; i++) {
+	for(int j =1; j<=i; j++) {
+		System.out.print(i);
+	}
+	System.out.println();
+}
+}
+}
