4a88a1a7a66e25371b81ce198e2ff25b9d29eedc
Snehal Adbol <smitaadbol01@gmail.com>
2023-11-14 01:51:06 +0530

    init

---
+++ 10th/arrays/selection_sort1.java
@@ -0,0 +1,45 @@
+package tenth;
+import java.util.*; 
+
+
+// basic selection sort 
+
+public class array1 {
+	
+	// creating a function to print the array 
+
+	public static void printArray(int arr[]) { // remember to make this func static 
+		int len = arr.length ; 
+		for(int i = 0; i <=len-1; i++) {
+		System.out.println(arr[i]); 
+	}
+	}
+	// static method - The static keyword is used to construct methods that will 
+	// exist regardless of whether or not any instances of the class are generated.
+	
+	public static void main(String[]args) {
+		
+		int arr[] = {1,6,7,2,5}; 
+		
+		int len = arr.length; 
+		
+		for(int i = 0; i<=len-1; i++){
+			int smallest = i;
+			for(int j = i+1; j<len; j++) {
+				if(arr[smallest] > arr[j]) {
+					
+					smallest = j ; 
+				}
+				// time for a nice swap :)
+				int temp = arr[smallest]; 
+				arr[smallest] = arr[i]; 
+				arr[i] = temp; 
+			}
+		}
+		
+		printArray(arr);
+		
+	}
+}
+	
+
