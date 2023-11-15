a0d4fd05a688d9fc5d8433d0cc9026c8772d2cc2
Snehal Adbol <smitaadbol01@gmail.com>
2023-11-16 01:24:38 +0530

    init

---
+++ 10th/arrays/bubble_sort2.java
@@ -0,0 +1,43 @@
+package tenth;
+import java.util.*;
+
+//wap to sort the names of 'n' ppl based on their marks 
+// PG 189 note a)
+
+// 
+public class bubble_sort2 {
+	public static void main(String[]args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter total no. of students");
+		int num = sc.nextInt(); 
+		
+		String names[] = new String[num]; 
+		int  marks[] = new int[num]; 
+		
+		for(int i = 0; i<num; i++) {
+			System.out.println("Enter the name and their marks"); 
+			names[i] = sc.next(); 
+			marks[i] =sc.nextInt(); 
+		}
+		
+		// sorting using bubble sort 
+		for(int i = 0; i<num-1; i++) {
+			for(int j = 0;j <num-i-1; j ++) {
+				if(marks[j] > marks[j+1]) {
+					// swap  
+					int temp = marks[j];
+					marks[j] = marks[j+1]; 
+					marks[j+1] = temp; 	
+				}
+			}
+		}
+		
+		for(int i =0; i<num; i++) {
+			System.out.println( names[i]+ " " +marks[i]);
+		}
+		
+		}
+	
+	
+	
+	}
