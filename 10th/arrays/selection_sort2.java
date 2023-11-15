ea25fab2c78754c12c6a9c705d3505798473b913
Snehal Adbol <smitaadbol01@gmail.com>
2023-11-16 01:56:42 +0530

    init

---
+++ 10th/arrays/selection_sort2.java
@@ -0,0 +1,48 @@
+package tenth;
+import java.util.*;
+
+//wap to sort the names and telephones no. of 'n' ppl and sort them in ascending order 
+// PG 189 note b)
+// uisng selection sort 
+
+// 
+public class selection_sort2 {
+	public static void main(String[]args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter total no. of people");
+		int num = sc.nextInt(); 
+		
+		String names[] = new String[num]; 
+		int  nums[] = new int[num]; 
+		
+		for(int i = 0; i<num; i++) {
+			System.out.println("Enter the name and their phone no."); 
+			names[i] = sc.next(); 
+			nums[i] =sc.nextInt(); 
+		}
+		
+		// selection sort
+		for(int i =0; i<num-1; i++) {
+			int smallest = i; 
+			for(int j = i+1; j<num; j++) {
+				if(nums[smallest] > nums[j]) {
+					
+					smallest = j; 
+				}
+				
+				// swap
+				int temp = nums[smallest]; 
+				nums[smallest] = nums[j];
+				nums[j] = temp;
+			}
+		}
+		
+		for(int i =0; i<num; i++) {
+			System.out.println( names[i]+ " " +nums[i]);
+		}
+		
+		}
+	
+	
+	
+	}
