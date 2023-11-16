07ecd96f7e912d0136cfea52a4789f4db62daa36
Snehal Adbol <smitaadbol01@gmail.com>
2023-11-16 13:01:45 +0530

    init

---
+++ 10th/String/ascci.java
@@ -0,0 +1,19 @@
+package tenth;
+import java.util.*;
+
+// wap  to get the ascii value of chars 'a' and 'F'
+// Q3 Pg 223
+public class ascci {
+	public static void main(String[]args) {
+			char ch1 = 'a'; 
+			char ch2 ='F'; 
+			
+			 int a = ch1; 
+			 int b = ch2; 
+			 System.out.println(a);
+			 System.out.println(b);
+		}
+	
+	
+	
+	}
