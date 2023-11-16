b5c0685549c30ceceb68f0bba14bd75a4888594b
Snehal Adbol <smitaadbol01@gmail.com>
2023-11-16 13:07:27 +0530

    init

---
+++ 10th/String/ends1.java
@@ -0,0 +1,17 @@
+package tenth;
+import java.util.*;
+
+// wap  to check whether a  given string ends with the contents of another string 
+// Q5 Pg 223
+public class ends1 {
+	public static void main(String[]args) {
+			String org = "Helllo people";
+			String ends = "people";
+			
+			boolean b = org.endsWith(ends);
+			 System.out.println(b);
+		}
+	
+	
+	
+	}
