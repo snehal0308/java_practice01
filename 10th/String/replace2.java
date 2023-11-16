3d6b4cdc5186d35004dc9e7fbf749ce7aad2e93c
Snehal Adbol <smitaadbol01@gmail.com>
2023-11-16 13:35:56 +0530

    jiit

---
+++ 10th/String/replace2.java
@@ -0,0 +1,16 @@
+package tenth;
+import java.util.*;
+
+// wap 
+// Q6 Pg 223
+public class replace2 {
+	public static void main(String[]args) {
+			String str1 = "This is String Example"; 
+			str1 = str1.replace("This", "It"); 
+			System.out.println(str1); 
+			
+		}
+	
+	
+	
+	}
